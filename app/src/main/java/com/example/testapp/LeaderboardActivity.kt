6a4074db9462ddec49787be2d98d96be8ca292ef
zoe-cough <85243009+zoe-cough@users.noreply.github.com>
2022-12-11 20:19:28 -0500

    leaderboard back button

--- app/src/main/java/com/example/testapp/LeaderboardActivity.kt
+++ app/src/main/java/com/example/testapp/LeaderboardActivity.kt
@@ -1,8 +1,11 @@
 package com.example.testapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,16 +17,26 @@ import com.parse.ParseUser
 class LeaderboardActivity : AppCompatActivity() {
     lateinit var lbRecyclerView: RecyclerView
     lateinit var adapter: LeaderboardAdapter
+    lateinit var back : ImageButton
     var allScores: MutableList<Leaderboard> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
+        back = findViewById(R.id.id_backBtn2)
         val set: StudySet? = intent.getParcelableExtra("set")
         lbRecyclerView = findViewById(R.id.rvLeaderboard)
         adapter = LeaderboardAdapter(this, allScores)
         lbRecyclerView.adapter = adapter
         lbRecyclerView.layoutManager = LinearLayoutManager(this)
+
+            // back button
+        back.setOnClickListener {
+            val intent = Intent(this , TestActivity::class.java)
+            intent.putExtra("set", set)
+            startActivity(intent)
+        }
+
         queryLeaderboard(set)
         if (set != null) {
             findViewById<TextView>(R.id.tvSetname).text = set.getSetName()
